370b6b04a8af4db18eb53354f9ed53194fe0ce96
Benjamin Marsh <bmarsh@lisd.us>
2023-01-01 15:20:05 -0500

    fixes

--- main.py
+++ main.py
@@ -1,21 +1,59 @@
 import discord
 import dotenv
 import os
+import sentry_sdk
 
 dotenv.load_dotenv()
 
 token = os.getenv("TOKEN")
-client=discord.Client(intents=discord.Intents.all())
+client = discord.Client(intents=discord.Intents.all())
+
+sentry_sdk.init(
+    dsn= "https://fd0c4fb898af479b9a25e06df1fcc335@o1236511.ingest.sentry.io/4504282752811008",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate= 1.0,
+    _experiments= {
+        "profiles_sample_rate": 1.0
+    },
+    auto_enabling_integrations= True
+)
 
 @client.event
 async def on_ready():
-  print(f'{client.user.name} is online!')
+    print(f'{client.user.name} is online!')
 
+
+@client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  elif message.content.startswith("!hello") or message.content.startswith("/hello"):
-    await message.channel.send("Hello is an odd greeting.")
+    if not message.content.startswith("?"):
+        return
+
+    match message.content.replace("?", ""):
+        # What does this do?
+        # It checks if the message starts with "?"
+        # If it does, it removes the "?" and then checks the message
+        # If the message is "hello", it will send "Hello!"
+        # If the message is "bye", it will send "Bye!"
+        # If the message is "ping", it will send "pong"
+        # If the message is anything else, it will send "I don't understand"
+        case "hello":
+            await message.channel.send("Hello!")
+            return
+        case "bye":
+            await message.channel.send("Bye!")
+            return
+        case "ping":
+            await message.channel.send("pong")
+            return
+        case _:
+            await message.channel.send("I don't understand")
+            return
 
 client.run(token)
+
+1/0
